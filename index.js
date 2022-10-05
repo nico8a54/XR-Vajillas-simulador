9f209389e11d4f8c5f5aef0b24351598f9d49aec
nico8a54 <nico8a54@hotmail.com>
2022-10-04 20:10:33 -0500

    1

---
+++ index.js
@@ -0,0 +1,6 @@
+function changeToWhite () {
+    document.getElementById("XR").src="https://nico8a54.github.io/XR-Vajillas-corona-2/";
+}
+function changeToPurple () {
+    document.getElementById("XR").src="https://nico8a54.github.io/XR-Vajillas-corona/";
+}
